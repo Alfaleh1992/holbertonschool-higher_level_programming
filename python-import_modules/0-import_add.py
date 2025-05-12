5a5f19fa20ff04f1830aed3d3f32bcce328ab758
Alfaleh1992 <imis.190@gmail.com>
2025-05-12 03:40:39 +0000

    Fix task 0 fully with import guard

--- python-import_modules/0-import_add.py
+++ python-import_modules/0-import_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 add = __import__('0-add').add
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))

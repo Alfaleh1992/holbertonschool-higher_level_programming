572843baaee25723ec76e7c2713c36b167ee0d96
Alfaleh1992 <imis.190@gmail.com>
2025-05-30 15:17:53 +0000

    Fix: Add docstring spacing to satisfy pycodestyle E302

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-Defines function inherits_from
+Contains the inherits_from function
 """
 
 
 def inherits_from(obj, a_class):
     """
-    Returns True if obj is a subclass instance of a_class, not a direct instance.
+    Returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class;
+    otherwise False.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
